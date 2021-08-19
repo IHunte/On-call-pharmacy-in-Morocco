ffc056d873a9d9d2634348ca742df79c36340cd1
IHunte <ihunte.dev@gmail.com>
2021-08-19 23:26:32 +0100

    - display cities in select option
    - add addEventListener for selected option
    
    - add function CallAPI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,6 @@
 <!--
 inline-flex flex-col space-y-52 items-end justify-end pl-96 pt-80 bg-gradient-to-b from-yellow-200 to-white" style="width: 1440px; height: 1024px;
 -->
-<header class="text-gray-600 body-font">
-
-  </header>
 
 <body>
     <div class="bg-gradient-to-b from-blue-400 via-purple-300 to-yellow-100">
@@ -13,37 +10,33 @@ inline-flex flex-col space-y-52 items-end justify-end pl-96 pt-80 bg-gradient-to
           <!-- header -->
           <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center p-4">
             <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-              </svg>
+                <img src="{{ asset('img/drapeau-maroc.png') }}" alt="" srcset=""
+                class="rounded-full h-20 w-20 flex items-center justify-center">
               <span class="ml-3 text-xl text-red-100">On-Call Pharmacy</span>
             </a>
           </div>
           <div class="flex-grow flex flex-row overflow-hidden justify-center">
             <!-- lhs -->
-            <div class="flex-shrink-0 w-1/4 p-4">
-              Left menu
-            </div>
+            <div class="flex-shrink-0 w-1/4 p-4"></div>
             <!-- center -->
             <div class="flex-1 flex flex-col mt-8">
 
             <!-- Search -->
             <form action="" method="post">
                 <div class="relative m-4 pb-2 ">
-                    <input
+                    <select
                       type="text"
+                      id="fromCity"
                       class="h-14 w-full pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none"
                       placeholder="Search anything...">
+                      <option disabled selected value> -- Select a city -- </option>
+                    </select>
 
-                  <div class="absolute top-4 right-3">
-                      <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
-                  </div>
                 </div>
 
             </form>
 
-
-              <main class="flex-1 overflow-y-auto p-4 ">
+              <main class="flex-1 overflow-y-auto p-4">
                 <div class="relative">
                   <!-- Card  -->
                   <div class="bg-white shadow-lg rounded-3xl overflow-hidden m-4 sm:flex border border-gray-600">
@@ -86,9 +79,7 @@ inline-flex flex-col space-y-52 items-end justify-end pl-96 pt-80 bg-gradient-to
               </div>
             </main>
             <!-- rhs -->
-            <div class="flex-shrink-0 w-1/4 p-4">
-              Right sidebar
-            </div>
+            <div class="flex-shrink-0 w-1/4 p-4"></div>
           </div>
           <!-- footer -->
           <div class=" p-4">
@@ -96,5 +87,85 @@ inline-flex flex-col space-y-52 items-end justify-end pl-96 pt-80 bg-gradient-to
           </div>
         </div>
       </div>
+
+      <script>
+        let cities = [
+            'Casablanca',
+            'Marrakech',
+            'Agadir',
+            'Meknes',
+            'Taourirt',
+            'Safi',
+            'Fes',
+            'Rabat',
+            'Nador',
+            'Berkane',
+            'Tanger',
+            'Sefrou',
+            'Oujda',
+            'Khouribga',
+            'Beni Mellal',
+            'Guelmim',
+            'Mohammedia',
+            'Taza',
+            'El Jadida',
+            'Sale',
+            'Laayoune',
+            'Tetouan',
+            'Berrechid',
+            'Ouarzazate',
+            'Tan Tan',
+            'Ben Guerir',
+            'Ait Melloul',
+            'Settat',
+            'El Kelaa Des Sraghna',
+            'Chefchaouen',
+            'Temara',
+            'Kenitra',
+            'Errachidia',
+            'Souk El Arba Du Gharb',
+            'Al Hoceima',
+            'Larache',
+            'Tikiouine',
+            'Fkih Ben Salah',
+            'Tahla',
+            'Bouknadel',
+            'Sidi Slimane',
+            'Selouane',
+            'Azrou',
+            'Al Arrui',
+            'Ksar El Kebir',
+            'Zghanghan',
+            'Tiflet',
+            'Sidi Kacem'
+        ];
+        citySelect = document.getElementById('fromCity');
+
+        for (var city in cities) {
+          citySelect.options[citySelect.options.length] = new Option(cities[city], cities[city]);
+        }
+
+        function CallAPI()
+        {
+            console.log(citySelect.value);
+            fetch('http://localhost:8000/api/pharmacy/' + citySelect.value).then(function (response) {
+                // The API call was successful,
+                return response.json();
+            }).then(function (data) {
+                // This is the JSON from our response.
+                console.log(data);
+            }).catch(function (error) {
+                // There was an error
+                console.warn('Something went wrong.', err);
+            });
+        }
+
+        document.getElementById('fromCity').addEventListener('change', CallAPI);
+
+
+
+    </script>
 </body>
+
+
 
